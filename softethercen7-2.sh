665c520d1b92dbaa90b5c68cbc015aa5c607ef4d
Big_ort <adytamasurya@gmail.com>
2018-01-14 04:41:00 +0700

    Update softethercen7-2.sh

--- softethercen7-2.sh
+++ softethercen7-2.sh
@@ -18,6 +18,7 @@ echo "creating vpnserver service ................................"
 echo "...................................................."
 echo "...................................................."
 echo "...................................................."
+
 cd /usr/local/vpnserver
 chmod 755 /etc/init.d/vpnserver
 mkdir /var/lock/subsys
