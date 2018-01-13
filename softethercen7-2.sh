d9576b3a1bb0db75cf98542fa342329df0bc2829
Big_ort <adytamasurya@gmail.com>
2018-01-13 18:21:21 +0700

    Create softethercen7-2.sh

---
+++ softethercen7-2.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+echo "moving dir vpnserver................................"
+echo "...................................................."
+echo "...................................................."
+echo "...................................................."
+echo "...................................................."
+cd
+mv vpnserver /usr/local
+cd /usr/local/vpnserver
+chmod 600*
+chmod 700 vpncmd
+chmod 700 vpnserver
+cd /etc/init.d/
+wget https://raw.githubusercontent.com/BigOrt/cen7/master/vpnserver
+cd
+
+echo "creating vpnserver service ................................"
+echo "...................................................."
+echo "...................................................."
+echo "...................................................."
+
+chmod 755 /etc/init.d/vpnserver
+mkdir /var/lock/subsys
+update-rc.d vpnserver defaults
+chkconfig --add vpnserver
+chkconfig vpnserver on
+/etc/init.d/vpnserver start
+echo "running ./vpncmd ................................"
+echo "...................................................."
+echo "...................................................."
+echo "...................................................."
+./vpncmd
