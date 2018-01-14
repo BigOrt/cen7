f8dbd35e72b926c00728c5c4ea6a6aa6f5a6517e
Big_ort <adytamasurya@gmail.com>
2018-01-14 17:18:59 +0700

    Update upgrading php72cen7.sh

--- upgrading php72cen7.sh
+++ upgrading php72cen7.sh
@@ -3,11 +3,14 @@
 #upgrade php
 yum remove php-common mod_php php-cl
 
-yum-config-manager --enable remi-php71
-yum --enablerepo=remi,remi-php71 install php-fpm php-common
+#php7**** just change
+yum-config-manager --enable remi-php72
+yum --enablerepo=remi,remi-php72 install php-fpm php-common
 
-yum --enablerepo=remi,remi-php71 install php-opcache php-pecl-apcu php-cli php-pear php-pdo php-mysqlnd php-pgsql php-pecl-mongodb php-pecl-redis php-pecl-memcache php-pecl-memcached php-gd php-mbstring php-mcrypt php-xml
+yum --enablerepo=remi,remi-php72 install php-opcache php-pecl-apcu php-cli php-pear php-pdo php-mysqlnd php-pgsql php-pecl-mongodb php-pecl-redis php-pecl-memcache php-pecl-memcached php-gd php-mbstring php-mcrypt php-xml
 
 #check www.conf /etc/php-fpm.d/
 
 cp www.conf.rpmsave www.conf
+
+service php-fpm restart
