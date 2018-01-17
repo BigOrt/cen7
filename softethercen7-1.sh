9b3db798f25d735564647e4190551f082638e53f
Big_ort <adytamasurya@gmail.com>
2018-01-17 12:03:58 +0700

    Update softethercen7-1.sh

--- softethercen7-1.sh
+++ softethercen7-1.sh
@@ -15,4 +15,6 @@ echo "..................................................."
 wget http://www.softether-download.com/files/softether/v4.24-9652-beta-2017.12.21-tree/Linux/SoftEther_VPN_Server/32bit_-_Intel_x86/softether-vpnserver-v4.24-9652-beta-2017.12.21-linux-x86-32bit.tar.gz
 tar xzvf softether-vpnserver-v4.24-9652-beta-2017.12.21-linux-x86-32bit.tar.gz
 cd vpnserver
+echo "running make ......! just follow step [yes]..."
+echo ".............................................."
 make
