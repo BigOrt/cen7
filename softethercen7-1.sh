dd37f33d9c165f2dd0071119fad5113cce1e5b16
Big_ort <adytamasurya@gmail.com>
2018-01-17 20:39:22 +0700

    Update softethercen7-1.sh

--- softethercen7-1.sh
+++ softethercen7-1.sh
@@ -4,7 +4,7 @@ echo "................................"
 echo '.................................'
 yum update
 yum -y install epel-release
-yum groupinstall "Development Tools"
+#yum groupinstall "Development Tools"
 yum -y install gcc*
 yum -y install libpcap*
 yum -y install gcc zlib-devel openssl-devel readline-devel ncurses-devel
@@ -13,7 +13,7 @@ echo "downloading softether vpn .................."
 echo "................................................"
 echo "..................................................."
 wget http://www.softether-download.com/files/softether/v4.24-9652-beta-2017.12.21-tree/Linux/SoftEther_VPN_Server/32bit_-_Intel_x86/softether-vpnserver-v4.24-9652-beta-2017.12.21-linux-x86-32bit.tar.gz
-tar -xzvf softether-vpnserver-v4.24-9652-beta-2017.12.21-linux-x86-32bit.tar.gz
+tar xzvf softether-vpnserver-v4.24-9652-beta-2017.12.21-linux-x86-32bit.tar.gz
 cd vpnserver
 echo "running make ......! just follow step [yes]..."
 echo ".............................................."
