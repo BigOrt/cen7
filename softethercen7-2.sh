6ed224940d9abb8ada1c29ecc6397d1f67418827
Big_ort <adytamasurya@gmail.com>
2018-01-17 20:10:18 +0700

    Update softethercen7-2.sh

--- softethercen7-2.sh
+++ softethercen7-2.sh
@@ -7,7 +7,7 @@ echo "...................................................."
 cd
 mv vpnserver /usr/local
 cd /usr/local/vpnserver
-chmod 600*
+chmod 600 *
 chmod 700 vpncmd
 chmod 700 vpnserver
 cd /etc/init.d/
