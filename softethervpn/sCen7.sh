d3e4f4423518198a8b02c54f026c5c2b08d99a9a
Big_ort <adytamasurya@gmail.com>
2018-01-20 02:29:37 +0700

    Update sCen7.sh

--- softethervpn/sCen7.sh
+++ softethervpn/sCen7.sh
@@ -12,8 +12,21 @@ yum -y install gcc zlib-devel openssl-devel readline-devel ncurses-devel;
 echo ".................downloading softether vpn ....................";
 echo "...............................................................";
 echo "...............................................................";
-wget http://www.softether-download.com/files/softether/v4.25-9656-rtm-2018.01.15-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.25-9656-rtm-2018.01.15-linux-x64-64bit.tar.gz;
-tar xzvf softether-vpnserver-v4.25-9656-rtm-2018.01.15-linux-x64-64bit.tar.gz;
+echo "-----------------checking version-----------------------------";
+
+a="hostnamectl | grep architecture";
+echo "version : `eval $a` ";
+echo "----------------------------";
+echo "go....";
+if [ $a == "`eval $a`" ]
+then
+ wget http://www.softether-download.com/files/softether/v4.25-9656-rtm-2018.01.15-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.25-9656-rtm-2018.01.15-linux-x64-64bit.tar.gz;
+ tar xzvf softether-vpnserver-v4.25-9656-rtm-2018.01.15-linux-x64-64bit.tar.gz;
+else
+ http://www.softether-download.com/files/softether/v4.25-9656-rtm-2018.01.15-tree/Linux/SoftEther_VPN_Server/32bit_-_Intel_x86/softether-vpnserver-v4.25-9656-rtm-2018.01.15-linux-x86-32bit.tar.gz;
+ tar xzvf SoftEther_VPN_Server/32bit_-_Intel_x86/softether-vpnserver-v4.25-9656-rtm-2018.01.15-linux-x86-32bit.tar.gz
+fi
+
 cd vpnserver;
 echo "running make ......! just follow step [yes]...";
 echo "..............................................";
